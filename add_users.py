2599baf592d2e3617da56c16403d4813d075b714
horvath <patricia.horvath@tum.de>
2023-10-29 18:24:19 +0100

    added def and code cleanup

--- add_users.py
+++ add_users.py
@@ -7,8 +7,12 @@ import pandas as pd
 URL = 'https://psa.in.tum.de/xwiki/bin/view/PSA%20WiSe%202023%20%202024/Public%20Keys/'
 
 
-def remove_duplicates(x):
-    return list(dict.fromkeys(x))
+def check_group_exists(name):
+    try:
+        subprocess.run(['getent', 'group', name], check=True, capture_output=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
 
 
 def add_team_user_numbers_col(df):
@@ -28,34 +32,40 @@ def text_to_df(text):
     temp_tokenized = tokenized
     del temp_tokenized[:6]
     new_list = [temp_tokenized[x:x + 5] for x in range(0, len(temp_tokenized) - 2, 5)]
-    df = pd.DataFrame(new_list, columns=['lrz_id', 'username', 'gecos', 'team', 'pub_key'])
+    df = pd.DataFrame(new_list, columns=['username', 'gecos', 'team', 'pub_key', 'lrz_id'])
     df['team'] = [x.removeprefix("Team ") for x in df['team']]
     df['team'] = [int(x) for x in df['team']]
-    add_team_user_numbers_col(df)
+    df = add_team_user_numbers_col(df)
     return df
 
 
 def add_users(user_df):
+    temp_df = user_df[['username', 'team', 'member_nr']]
     # iterate over df and check if user exists
-    usernames = user_df['username'].tolist()
-    # usernames = remove_duplicates(usernames)
-    for user in usernames:
-        exists = subprocess.run(['grep', user, '/etc/passwd'], text=True)
-        # if not, create user with specific uid and primary gid + secondary gid
-        if exists.stdout == '':
-            print()
-            # adduser --disabled-password
-            # uid = 1000 + 100 * <team-nr> + <member-nr-in-team>
-            # gids = [1000 + (100 * int(x)) for x in user_df['team']]
-            # subprocess.run([])
+    for index, row in temp_df.iterrows():
+        try:
+            subprocess.run(['id', row['username']], check=True)
+            print(f"User '{username}' already exists.")
+            return
+        except subprocess.CalledProcessError:
+            pass
+        # calculate uid
+        uid = 1000 + 100 * int(row['team']) + int(row['member_nr'])
+        # calculate secondary gid, primary gid = uid
+        gid = 1000 + 100 * int(row['team'])
+        #         # adduser --disabled-password
+        subprocess.run(['sudo', 'useradd', '-u', str(uid), '-g', str(uid), row['username'], '--disabled-password'],
+                       check=True)
+
+        subprocess.run(['sudo', 'usermod', '-aG', str(gid), row['username']], check=True)
 
 
 if __name__ == "__main__":
     # check if psa group already exists, if not create new group
-    # exists = subprocess.run(['getent', 'group', 'psa2324'], capture_output=True, text=True)
-    # if exists.stdout == '':
-    #     subprocess.run(['sudo', 'groupadd', 'psa2324', '-g', '1099'], text=True)
-    username = "patricia.horvath@tum.de"
+    if not check_group_exists('psa2324'):
+        subprocess.run(['sudo', 'groupadd', 'psa2324', '-g', '1099'], text=True)
+
+    username = input("Input email: ")
     password = input("Input password: ")
 
     # parse user list from website and save to csv
@@ -65,6 +75,11 @@ if __name__ == "__main__":
     soup = bs(index_page.text, 'html.parser')
     results = soup.find(id="xwikicontent")
     temp_text = results.get_text("|")
+    # TODO temporary bug fix weil falscher input in der liste
+    temp_text = temp_text.replace(
+        '|ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFbetiUdtIMVZ+x0VR0PCdL+IOhcVu5CW++xbEIJZTGd domi-fresh@domifresh-hplaptop15dw1xxx|',
+        '')
+
     # format user data
     users = text_to_df(text=temp_text)
     # for every entry in user list create a new user if not already existing and add to group psa
