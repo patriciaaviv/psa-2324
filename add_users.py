c6865e87ebc221dc804d95f740ace50818a4c4e0
horvath <patricia.horvath@tum.de>
2023-11-28 17:25:56 +0100

    bug fix

--- add_users.py
+++ add_users.py
@@ -90,7 +90,7 @@ def add_users(user_df):
                 print(f"Failed to change password for user {row['username']}. Return code: {rc}")
             # create ssh directory
             filepath = f'/home/{row["username"]}'
-            if not os.path.exists(f'filepath/.ssh'):
+            if not os.path.exists(f'{filepath}/.ssh'):
                 os.mkdir(filepath)
             os.chmod(f'{filepath}/.ssh', 0o700)
             # add user key
