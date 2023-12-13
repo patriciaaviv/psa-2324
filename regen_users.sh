a0895179ffaa113759b3f46dc256d9670c527b49
Ida Kastlunger <91760131+CamelotMacaw@users.noreply.github.com>
2023-12-13 15:12:57 +0100

    Update regen_users.sh
    updated user array

--- regen_users.sh
+++ regen_users.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # List of users to delete (separated by space)
-users_to_delete=("schoe" "steph" "pahll" "frisc" "zette" "klaku" "chris" "tongu" "haugs" "yesse" "stoec" "wittm" "grote" "deike" "meuse" "schub" "wothg" "songl" "horva" "kastl")
+# chris and yesse are gone now, but still in here in case you run this on old machine that still has them
+users_to_delete=("schoe" "steph" "pahll" "frisc" "zette" "klaku" "chris" "dietr" "tongu" "haugs" "yesse" "prinz" "stoec" "wittm" "grote" "deike" "meuse" "schub" "wothg" "songl" "horva" "kastl")
 # Iterate through the list of users to delete
 for username in "${users_to_delete[@]}"; do
     # Check if the user exists
